2a7235632d3cdcc13da1bf7131d5cc5d44510061
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-02-08 10:33:39 -0500

    DONE

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,13 @@
 # Write your code here
+# need to get access to each word in the sentence: 1. split the string on "" 
+# reverse each word and add it to a new array
+# join the array of words on "" to create one string 
+
+require 'pry'
+
+def reverse_each_word(sentence)
+  sentence.split.map(&:reverse).join(" ")
+end 
+
+binding.pry
+0
